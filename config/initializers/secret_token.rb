eb1e5fab8cf1796ba3f2399cee2cf878d0a33962
Tim Dussinger <tritowntim@gmail.com>
2013-01-10 19:48:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repodepot::Application.config.secret_token = 'f66ce74dd329b8a8d8eb198c06b6406db30bcfaf2383204292429392085de3096a6dece615b743936886b8838a9bfe9423b9efc5b6ebbf019cca9f339b788ccb'
