14698ad9d522af6b99f10177d143302b8c843759
Tim Dussinger <tritowntim@gmail.com>
2013-01-16 23:58:24 -0500

    Basic List#edit form and links between List pages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
   end
 
   def show
-  	@list = List.find(params[:id])
+  	@list = find_list
   end
 
   def new
@@ -14,9 +14,25 @@ class ListsController < ApplicationController
   def create
     @list = List.new(params[:list])
     @list.save
+    # todo if save fails
     redirect_to @list
   end
 
   def edit
+    @list = find_list
   end
+
+  def update
+    @list = find_list
+    if @list.update_attributes(params[:list])
+      redirect_to @list
+    else
+      render :edit
+    end
+  end
+
+  private 
+    def find_list
+      List.find(params[:id])
+    end
 end
