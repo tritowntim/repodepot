0b23de07f141c7393ca3c2f7bf0d5aafa8506561
Tim Dussinger <tritowntim@gmail.com>
2013-01-24 18:05:06 -0500

    Listing ordering now populating during add and remove via js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,29 +16,29 @@
 
 $(function() { 
 
-		$('form').on('click', '.remove_field', function(event) {
-			event.preventDefault()
-			console.log('.remove_field clicked')
-			console.log($(event.target))
-			$(event.target).prev('input[type=hidden]').val('1')
-			$(event.target).closest('fieldset').hide()
-		})
+    // $('form').on('click', '.remove_field', function(event) {
+    //     event.preventDefault()
+    //     console.log('.remove_field clicked')
+    //     console.log($(event.target))
+    //     $(event.target).prev('input[type=hidden]').val('1')
+    //     $(event.target).closest('fieldset').hide()
+    // })
 
-    $('form').on('click', '.add_fields', function(event) {
-    	event.preventDefault()
-    	console.log('.add_fields clicked')
-    	var time = new Date().getTime()
-    	var regexp = new RegExp($(event.target).data('id'),'g')
-    	$(event.target).before($(this).data('fields').replace(regexp,time))
-    })
+    // $('form').on('click', '.add_fields', function(event) {
+    //     event.preventDefault()
+    //     console.log('.add_fields clicked')
+    //     var time = new Date().getTime()
+    //     var regexp = new RegExp($(event.target).data('id'),'g')
+    //     $(event.target).before($(this).data('fields').replace(regexp,time))
+    // })
 
     // $('form').on('click', '.add_listing', function(event) {
-    // 	event.preventDefault()
-    // 	console.log('.add_fields clicked')
-    // 	var time = new Date().getTime()
-    // 	var b = $("#blank-listing").html()
-    // 	var regexp = new RegExp($(b).data('id'),'g')
-    // 	$(event.target).before($("#blank-listing").html().replace(regexp,time))
+    //  event.preventDefault()
+    //  console.log('.add_fields clicked')
+    //  var time = new Date().getTime()
+    //  var b = $("#blank-listing").html()
+    //  var regexp = new RegExp($(b).data('id'),'g')
+    //  $(event.target).before($("#blank-listing").html().replace(regexp,time))
     // })  
 
     $('form').on('click', '.add_listing', function(event) {
@@ -55,6 +55,19 @@ $(function() {
     $('.cancel-repo-search').on('click', function(event) { 
       $('.repository-search').toggleClass('invisible-ui')
     })
+
+    $('form').on('click', '.remove_field', function(event) {
+        event.preventDefault()
+        console.log('.remove_field clicked')
+        console.log($(event.target))
+        $(event.target).prev('input[type=hidden]').val('1')
+        $(event.target).closest('fieldset').hide()
+
+        // if $(this).parent().css('display') == 
+        $('fieldset .ordering').each(function(i) { console.log($(this).parent().css('display')) } )
+        //$('fieldset .ordering').filter(function(i) { $(this).parent().css('display') == 'block' }).each(function(i) { $(this).val(i) } )
+        $('fieldset .ordering').filter(function(i) { return $(this).parent().css('display') == 'block' }).each(function(i) { $(this).val(i+1) } )
+    })    
 
 })
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   attr_accessible :description, :title, :listings_attributes
-  has_many :listings
+  has_many :listings, :order => "ordering"
   has_many :repositories, :through => :listings
 
   accepts_nested_attributes_for :listings, :allow_destroy => true
