90dd047da0fa372d5d1d06e25a119331bf58056b
Tim Dussinger <tritowntim@gmail.com>
2013-01-29 14:31:39 -0500

    Home page showing example lists from seed data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def home
+  	@throwdown_list = List.where("title = ?", "LinkedIn's Client-Side Templating Throwdown").first
+  	@trello_list = List.where("title = ?", "The Trello Stack").first
+  	@github_api_list = List.where("title = ?", "GitHub API Wrappers").first
   end
 end
