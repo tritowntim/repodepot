8b7bb9161d035768811f695243b9704746a97bab
Tim Dussinger <tritowntim@gmail.com>
2013-01-30 13:31:07 -0500

    GithubRepo API class now throwing proper exceptions; test specs updated accordingly

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -1,34 +1,42 @@
 class GithubRepo
 
-  attr_reader :owner, :repo_name, :gh, :repository
+  attr_reader :owner, :repo_name, :gh, :repository, :input
 
   def initialize(input)
-    create_repo if validate(input) 
+    @input = input
+    create_repo if valid_input?
   end
 
-  def validate(input) 
-    full_repo_name = input.gsub("https://github.com/","")
+  def valid_input?  
+    full_repo_name = @input.gsub("https://github.com/","")
     if full_repo_name.count("/") == 1
       # repo_ids = input[19,input.length-1].split("/")
       repo_ids = full_repo_name.split("/")
       @owner, @repo_name = repo_ids[0], repo_ids[1]
-      @owner.length > 0 && @repo_name.length > 0 
+      if @owner.length > 0 && @repo_name.length > 0
+        true
+      else
+        raise_invalid_input_format 
+      end
     else 
-      raise ArgumentError, "Repository name/URL of '#{input}' has incorrect format; see examples below"
+      raise_invalid_input_format  
     end
-    # todo: throw error if invalid
-   end 
+  end 
 
-  def lookup 
+  def raise_invalid_input_format
+    raise ArgumentError, "Repository name/URL of '#{@input}' has incorrect format; see examples below"
+  end
+
+  def lookup
+
+    # todo: replace with OAuth somehow, and/or use logged-in user's account for API calls
     begin
-      # todo: replace with OAuth somehow, and/or use user's account 
       github = Github.new :login => ENV['GITHUB_USERNAME'], :password => ENV['GITHUB_PASSWORD']
       @gh = github.repos.get @owner, @repo_name 
-      puts "RATE LIMIT REMAINING = " + @gh.ratelimit_remaining
-      true
+      puts "GitHub API Rate Limit: remaining API calls allowed = #{@gh.ratelimit_remaining}"
+
     rescue Github::Error::NotFound => e
-      puts "GHERROR = " + e.message
-      false
+      raise ArgumentError, "GitHub repository '#{@input}' not found "
     end  
   end 
 
@@ -38,28 +46,25 @@ class GithubRepo
       @repository.owner_login = @owner
       @repository.name = @repo_name
 
-      if lookup
+      lookup
 
-        # todo: easier way to write with block?
-        @repository.github_id = @gh.id
-        @repository.full_name = @gh.full_name
-        @repository.gh_created_at = @gh.created_at
-        @repository.gh_updated_at = @gh.updated_at
-        @repository.pushed_at = @gh.pushed_at
-        @repository.watchers_count = @gh.watchers_count
-        @repository.forks_count = @gh.forks_count
-        @repository.open_issues_count = @gh.open_issues_count
-        @repository.network_count = @gh.network_count
-        @repository.language = @gh.language
-        @repository.description = @gh.description
-        @repository.clone_url = @gh.clone_url
-        @repository.html_url = @gh.html_url
+      # todo: easier way to write with block?
+      @repository.github_id = @gh.id
+      @repository.full_name = @gh.full_name
+      @repository.gh_created_at = @gh.created_at
+      @repository.gh_updated_at = @gh.updated_at
+      @repository.pushed_at = @gh.pushed_at
+      @repository.watchers_count = @gh.watchers_count
+      @repository.forks_count = @gh.forks_count
+      @repository.open_issues_count = @gh.open_issues_count
+      @repository.network_count = @gh.network_count
+      @repository.language = @gh.language
+      @repository.description = @gh.description
+      @repository.clone_url = @gh.clone_url
+      @repository.html_url = @gh.html_url
 
-        @repository.save
-        @repository
-      else
-        nil
-      end
+      @repository.save
+      @repository  
 
     end
 
--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -2,39 +2,37 @@ require 'spec_helper'
 
 require_relative '../../app/models/github_repo'
 
-describe "GithubRepo" do 
-
-	gh = nil
-	before(:all) { gh = GithubRepo.new("https://github.com/twitter/bootstrap") }
+describe "GithubRepo" do  
 
 	describe "create new repo" do
 
-		it "creates new repository record" do
-			repo = gh.repository
+		it "builds new repository model with URL" do
+			repo = GithubRepo.new("https://github.com/twitter/bootstrap").repository
 			expect(repo.full_name).to eq "twitter/bootstrap"
 			expect(repo.github_id).to eq 2126244
+		end 
+
+		it "builds new repository model with repo name" do
+			repo = GithubRepo.new("jquery/jquery").repository
+			expect(repo.full_name).to eq "jquery/jquery"
+			expect(repo.github_id).to eq 167174
+		end 
+
+		it "errors for invalid input format for URL" do 
+			# http instead of https
+			expect {GithubRepo.new("http://github.com/twitter/bootstrap")}.to raise_error(ArgumentError)
+		end 
+
+		it "errors for invalid input format for repo name" do 
+			expect {GithubRepo.new("twitter bootstrap")}.to raise_error(ArgumentError)
 		end
 
-		it "validates valid repo name input" do 
-			expect(gh.validate("twitter/bootstrap")).to eq true
+		it "errors on GitHub 404 for URL" do 
+			expect {GithubRepo.new("http://github.com/rails/rails3")}.to raise_error(ArgumentError)
 		end
 
-		it "finds invalid repo name input" do 
-			expect {gh.validate("twitter bootstrap")}.to raise_error(ArgumentError)
-		end
-
-		it "validates URL input" do 
-			expect(gh.validate("https://github.com/twitter/bootstrap")).to eq true
-		end
-
-		it "finds invalid URL input" do 
-			expect {gh.validate("https://gh.com/twitter/bootstrap")}.to raise_error(ArgumentError)
-		end
-
-		it "handles repo-not-found gracefully" do 
-			repo = GithubRepo.new("twitter/facebook")
-			expect(repo.repository.id).to eq nil
-			expect(repo.repository.github_id).to eq nil
+		it "errors on GitHub 404 for repo name" do 
+			expect {GithubRepo.new("rails/rails4")}.to raise_error(ArgumentError)
 		end
 
 	end
