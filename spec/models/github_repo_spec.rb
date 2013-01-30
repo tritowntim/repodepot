1fe082abae33a6ceb84dd4fe289f6550bde3025a
Tim Dussinger <tritowntim@gmail.com>
2013-01-30 12:41:33 -0500

    Additional specs for Github API

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -3,16 +3,33 @@ require 'spec_helper'
 require_relative '../../app/models/github_repo'
 
 describe "GithubRepo" do 
-	
+
+	gh = nil
+	before(:all) { gh = GithubRepo.new("https://github.com/twitter/bootstrap") }
+
 	describe "create new repo" do
 
-		it "returns new repository" do
-
-			gh = GithubRepo.new("https://github.com/twitter/bootstrap")
+		it "creates new repository record" do
 			repo = gh.repository
 			expect(repo.full_name).to eq "twitter/bootstrap"
 			expect(repo.github_id).to eq 2126244
-
 		end
+
+		it "validates repo name input" do 
+			expect(gh.validate("twitter/bootstrap")).to eq true
+			expect(gh.validate("twitter bootstrap")).to eq false
+		end
+
+		it "validates URL input" do 
+			expect(gh.validate("https://github.com/twitter/bootstrap")).to eq true
+			expect(gh.validate("https://gh.com/twitter/bootstrap")).to eq false
+		end
+
+		it "handles repo-not-found gracefully" do 
+			repo = GithubRepo.new("twitter/facebook")
+			expect(repo.repository.id).to eq nil
+			expect(repo.repository.github_id).to eq nil
+		end
+
 	end
 end
