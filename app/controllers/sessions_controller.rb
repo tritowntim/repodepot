d1f5bcb581d5df237b0c0a8203bbde123a97cbd0
Tim Dussinger <tritowntim@gmail.com>
2013-02-07 15:21:44 -0500

    Remove commented-out from session controller, add todo's for flashing log in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
 	
 	def create
-		# hash of details about authenticated user
-		# raise env["omniauth.auth"].to_json
+		# env["omniauth.auth"] is hash of details about authenticated user
 	  user = User.from_omniauth(env["omniauth.auth"])
 	  session[:user_id] = user.id
-	  #redirect_to root_url, :notice => "Logged in as #{user.name}!"
+	  # todo: implement redirect_to root_url, :notice => "Logged in as #{user.name}!"
 	  redirect_to request.env['omniauth.origin'] || root_url
 	end
 
 	def destroy
 		if session[:user_id] != nil
 		  session[:user_id] = nil
+		  # todo: notice logged out
 		  redirect_to request.env['HTTP_REFERER'], :notice => "Logged out"		
 		end
 	end
